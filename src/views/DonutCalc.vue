834b0be9caadc5f5b4cb62e2e8b19452ad82e8df
jHaben <jrhaben@gmail.com>
2022-04-05 15:35:14 -0500

    add number pad

--- src/views/DonutCalc.vue
+++ src/views/DonutCalc.vue
@@ -6,104 +6,162 @@
 
     <v-sheet height="20" color="rgb(211,62,39,1)" />
     <v-container>
-      <base-card>
-        <div style="font-size: 25px" class="ma-2">
-          <v-row>
-            <v-col
-              ><v-img
-                max-height="50"
-                max-width="50"
-                src="../assets/donutCalc/donut.png"
-              >
-              </v-img>
-            </v-col>
-            <v-col>
-              <v-btn>-</v-btn>
-            </v-col>
-            <v-col>
-              <v-btn> +</v-btn>
-            </v-col>
-            <v-col>0</v-col>
-          </v-row>
-          <v-row>
-            <v-col
-              ><v-img
-                max-height="50"
-                max-width="50"
-                src="../assets/donutCalc/bsandwhich.png"
-              >
-              </v-img>
-            </v-col>
-            <v-col>
-              <v-btn>-</v-btn>
-            </v-col>
-            <v-col>
-              <v-btn> +</v-btn>
-            </v-col>
-            <v-col>0</v-col>
-          </v-row>
-          <v-row>
-            <v-col
-              ><v-img
-                max-height="50"
-                max-width="50"
-                src="../assets/donutCalc/coffee.png"
-              >
-              </v-img>
-            </v-col>
-            <v-col>
-              <v-btn>-</v-btn>
-            </v-col>
-            <v-col>
-              <v-btn> +</v-btn>
-            </v-col>
-            <v-col>0</v-col>
-          </v-row>
-          <v-row>
-            <v-col
-              ><v-img
-                max-height="50"
-                max-width="50"
-                src="../assets/donutCalc/milk-juice.png"
-              >
-              </v-img>
-            </v-col>
-            <v-col>
-              <v-btn>-</v-btn>
-            </v-col>
-            <v-col>
-              <v-btn> +</v-btn>
-            </v-col>
-            <v-col>0</v-col>
-          </v-row>
-          <v-sheet height="15" />
-          <v-divider />
-          <v-sheet height="15" />
-          <v-row
-            ><v-col align="center"><strong>$ Total:</strong></v-col></v-row
-          >
-          <v-sheet height="15" />
-          <v-divider />
-          <v-sheet height="15" />
-          <v-row>
-            <v-col>
-              <v-text-field
-                label="Paid:"
-                value="0.00"
-                prefix="$"
-              ></v-text-field>
-            </v-col>
-          </v-row>
-          <v-row
-            ><v-col align="center"><strong>Change:</strong></v-col></v-row
-          >
-          <v-divider />
-          <v-sheet height="15" />
-          <v-row
-            ><v-col align="right"><v-btn><strong>Reset</strong></v-btn></v-col></v-row
-          >
-        </div>
-      </base-card></v-container
+      <center>
+        <base-card max-width="325" min-width="325">
+          <div style="font-size: 25px" class="ma-2">
+            <v-row>
+              <v-col
+                ><v-img
+                  max-height="50"
+                  max-width="50"
+                  src="../assets/donutCalc/donut.png"
+                >
+                </v-img>
+              </v-col>
+              <v-col>
+                <v-btn
+                  ><v-icon>{{ "mdi-minus" }}</v-icon></v-btn
+                >
+              </v-col>
+              <v-col>
+                <v-btn>
+                  <v-icon>{{ "mdi-plus" }}</v-icon></v-btn
+                >
+              </v-col>
+              <v-col>0</v-col>
+            </v-row>
+            <v-row>
+              <v-col
+                ><v-img
+                  max-height="50"
+                  max-width="50"
+                  src="../assets/donutCalc/bsandwhich.png"
+                >
+                </v-img>
+              </v-col>
+              <v-col>
+                <v-btn
+                  ><v-icon>{{ "mdi-minus" }}</v-icon></v-btn
+                >
+              </v-col>
+              <v-col>
+                <v-btn>
+                  <v-icon>{{ "mdi-plus" }}</v-icon></v-btn
+                >
+              </v-col>
+              <v-col>0</v-col>
+            </v-row>
+            <v-row>
+              <v-col
+                ><v-img
+                  max-height="50"
+                  max-width="50"
+                  src="../assets/donutCalc/coffee.png"
+                >
+                </v-img>
+              </v-col>
+              <v-col>
+                <v-btn
+                  ><v-icon>{{ "mdi-minus" }}</v-icon></v-btn
+                >
+              </v-col>
+              <v-col>
+                <v-btn>
+                  <v-icon>{{ "mdi-plus" }}</v-icon></v-btn
+                >
+              </v-col>
+              <v-col>0</v-col>
+            </v-row>
+            <v-row>
+              <v-col
+                ><v-img
+                  max-height="50"
+                  max-width="50"
+                  src="../assets/donutCalc/milk-juice.png"
+                >
+                </v-img>
+              </v-col>
+              <v-col>
+                <v-btn
+                  ><v-icon>{{ "mdi-minus" }}</v-icon></v-btn
+                >
+              </v-col>
+              <v-col>
+                <v-btn>
+                  <v-icon>{{ "mdi-plus" }}</v-icon></v-btn
+                >
+              </v-col>
+              <v-col>0</v-col>
+            </v-row>
+
+            <v-sheet height="15" />
+            <v-row
+              ><v-col align="center"><strong>Total: $</strong></v-col></v-row
+            >
+            <v-sheet height="15" />
+            <v-divider />
+            <v-sheet height="15" />
+            <v-row
+              ><v-col align="left"
+                ><strong>Amount Paid: $ 0.00 </strong></v-col
+              ></v-row
+            >
+            <v-row>
+              <v-col>
+                <v-btn rounded>7</v-btn>
+              </v-col>
+              <v-col>
+                <v-btn rounded>8</v-btn>
+              </v-col>
+              <v-col>
+                <v-btn rounded>9</v-btn>
+              </v-col>
+            </v-row>
+            <v-row>
+              <v-col>
+                <v-btn rounded>4</v-btn>
+              </v-col>
+              <v-col>
+                <v-btn rounded>5</v-btn>
+              </v-col>
+              <v-col>
+                <v-btn rounded>6</v-btn>
+              </v-col>
+            </v-row>
+            <v-row>
+              <v-col>
+                <v-btn rounded>1</v-btn>
+              </v-col>
+              <v-col>
+                <v-btn rounded>2</v-btn>
+              </v-col>
+              <v-col>
+                <v-btn rounded>3</v-btn>
+              </v-col>
+            </v-row>
+            <v-row>
+              <v-col> </v-col>
+              <v-col>
+                <v-btn rounded>0</v-btn>
+              </v-col>
+              <v-col> <v-btn rounded>C</v-btn></v-col>
+            </v-row>
+            <v-sheet height="15" />
+            <v-divider />
+            <v-sheet height="15" />
+            <v-row
+              ><v-col align="center"><strong>Change: $</strong></v-col></v-row
+            >
+            <v-divider />
+            <v-sheet height="15" />
+            <v-row
+              ><v-col align="right"
+                ><v-btn><strong>Reset</strong></v-btn></v-col
+              ></v-row
+            >
+          </div>
+        </base-card>
+      </center></v-container
     >
     <v-sheet height="20" color="rgb(235,243,243)" />
     <v-sheet height="75" color="rgb(235,243,243)" />
