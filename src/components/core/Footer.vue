2d6eadd84f7f127d21a37cf62ad13f330222c18c
jack <jrhaben@gmail.com>
2023-06-05 11:35:40 -0500

    email and calendar

--- src/components/core/Footer.vue
+++ src/components/core/Footer.vue
@@ -4,40 +4,33 @@
       <v-card-text text-center>
         <v-row justify="center" no-gutters>
           <v-col v-for="(link, i) in footerLinks" :key="i" sm="1">
-            <v-btn
-              v-if="!link.last & link.bottom"
-              class="mx-4 myBtn"
-              icon
-              :to="link.to"
-              :href="link.href"
-            >
+            <v-btn v-if="!link.last & link.bottom" class="mx-4 myBtn" icon :to="link.to" :href="link.href">
               <v-icon id="myIcon" @click="onClick($event, link)">
                 {{ link.icon }}
               </v-icon>
             </v-btn>
 
             <v-btn class="mx-4 myBtn" v-else-if="link.bottom" icon>
-              <ModalView :showIcon="status" />
+              <!-- <ModalView :showIcon="1" /> -->
+
+              <v-icon id="myIcon" @click="sendEmail">
+                {{ "mdi-email" }}
+              </v-icon>
+
             </v-btn>
           </v-col>
         </v-row>
       </v-card-text>
 
       <v-divider />
-      <v-card-text
-        class="py-2 white--text text-center"
-        color="rgb(235,243,243)"
-      >
+      <v-card-text class="py-2 white--text text-center" color="rgb(235,243,243)">
         <strong>Troop 121</strong> — {{ new Date().getFullYear() }}
       </v-card-text>
 
       <v-row>
         <v-col>
-          <v-btn class="mx-4 myBtn" icon :to="{ path: '/login' }"
-            ><v-icon id="myIcon">{{ "mdi-account-cog-outline" }}</v-icon></v-btn
-          ></v-col
-        ></v-row
-      >
+          <v-btn class="mx-4 myBtn" icon :to="{ path: '/login' }"><v-icon id="myIcon">{{ "mdi-account-cog-outline"
+          }}</v-icon></v-btn></v-col></v-row>
     </v-card>
   </v-footer>
 </template>
@@ -45,13 +38,13 @@
 <script>
 // Utilities
 import { mapGetters } from "vuex";
-import ModalView from "../modal/ModalView.vue";
+//import ModalView from "../modal/ModalView.vue";
 
 export default {
   name: "CoreFooter",
-  components: {
-    ModalView,
-  },
+  // components: {
+  //    ModalView,
+  // },
   data: () => ({
     status: true,
   }),
@@ -72,6 +65,10 @@ export default {
         this.$vuetify.goTo(item.href, { duration: 500, easing: "linear" });
       }
     },
+    sendEmail() {
+    window.location.href = 'mailto:troops@churchofsaintagnes.org';
   },
+  },
+
 };
 </script>
--- src/components/home/Contact.vue
+++ src/components/home/Contact.vue
@@ -35,3 +35,44 @@ export default {
   }),
 };
 </script>
+
+<template>
+  <v-container id="contact" tag="section">
+    <base-card> <v-row>
+        <v-col align="center">
+          <base-subheading>Contact Us</base-subheading></v-col>
+      </v-row>
+      <v-row>
+        <v-col align="center" class="text4 text-wrap ma-4">
+          <p>
+            To find out more information about our troop or about how to join
+            please send us an email by clicking below.
+          </p>
+        </v-col>
+      </v-row>
+      <v-col align="center" class="text4">
+        <v-btn @click="sendEmail" style="font-size: 25px"><v-icon color="black" size="30">mdi-email</v-icon>
+          E-Mail</v-btn>
+      </v-col>
+
+    </base-card>
+  </v-container>
+</template>
+  
+  
+  <script>
+  export default {
+  
+    name: "HomeContact",
+    methods: {
+      sendEmail() {
+        window.location.href = 'mailto:troops@churchofsaintagnes.org';
+      },
+    },
+  };
+  </script>
+  
+<style scoped>
+/* Add your styles here */
+</style>
+  
